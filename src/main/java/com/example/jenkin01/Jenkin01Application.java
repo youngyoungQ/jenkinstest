30c884dcc1561cc2e668f0f4d00b7341a14506a4
nickyoung <13123316361@163.com>
2020-03-19 21:24:28 +0800

    test

---
+++ src/main/java/com/example/jenkin01/Jenkin01Application.java
@@ -0,0 +1,13 @@
+package com.example.jenkin01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jenkin01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Jenkin01Application.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/jenkin01/controller/Controller.java
@@ -0,0 +1,19 @@
+package com.example.jenkin01.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+
+    public String get(){
+
+        return "get";
+    }
+
+
+
+}
+
+
