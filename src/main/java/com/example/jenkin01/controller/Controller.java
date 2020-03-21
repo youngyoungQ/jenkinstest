cd22f521da95d93dd4cb26eb2a784cc446b8c400
nickyoung <13123316361@163.com>
2020-03-21 23:15:14 +0800

    test

--- src/main/java/com/example/jenkin01/controller/Controller.java
+++ src/main/java/com/example/jenkin01/controller/Controller.java
@@ -1,12 +1,14 @@
 package com.example.jenkin01.controller;
 
+import org.springframework.boot.autoconfigure.validation.ValidationAutoConfiguration;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
 
-
+    @RequestMapping(value = "/get",method = RequestMethod.GET)
     public String get(){
 
         return "get";
